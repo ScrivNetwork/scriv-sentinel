27e45005938b00c1f6c846ea9b1cb796f64f71a9
realhagie <shagendorn@gmail.com>
2019-01-22 18:00:01 +0100

    case sensitive classname

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 import config
 
-from scrivd import scrivdaemon
+from scrivd import ScrivDaemon
 from scriv_config import ScrivConfig
 
 
